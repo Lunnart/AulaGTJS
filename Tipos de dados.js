e44df9d159fa9247f90dd03cd4889f322bb18ac6
Luanny <luanny>
2025-04-24 20:21:00 -0300

    Tipos de dados JS

---
+++ Tipos de dados.js
@@ -0,0 +1,51 @@
+// Tipos de dados primitivos
+
+// String
+
+const escola = 'Digial College'
+const curso = ""
+
+// Boolean ou Boleano (True or False)
+
+let x = 2
+let y = 4
+
+console.log(x > y)  //Operações de comparação output(saída) -> false
+
+// Number 
+
+idade = 30
+
+console.log(typeof(NaN)) //Not a Nunber = Não é um número
+
+// Undefined
+
+let nomeCompleto //camelCase
+
+console.log(nomeCompleto)
+
+//Null
+
+//Conversões
+
+let numeroAlunos = Number ("15")
+
+console.log(numeroAlunos)
+
+//Concatenação de string
+
+const nomeMae = "Mara"
+const sobrenomeMae = "Cristina"
+
+let nomeCompletoMae = 'O nome completo da minha mãe é ' nomeMae + '' + sobrenomeMae
+
+console.log(nomeCompletoMae)
+
+
+// Templet string (Usar acento grave `)
+ nomeCompletoMae = `
+ O nome da minha mãe é
+ ${nomeMae}
+ ${sobrenomeMae}
+
+ 
