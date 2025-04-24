e7cf096aee6df1ee578189d6e2f3734b9828ef0b
Luanny <luanny>
2025-04-24 19:39:45 -0300

    treinando JS

---
+++ index.js
@@ -0,0 +1,42 @@
+alert('Hello World!')
+
+// Declarar uma variável com o seu nome
+let name = 'Nayara';
+
+//Declarar uma variável com sua idade
+let idade = '29'
+
+//Declarar uma variável com a sua comida favorita e não atribuir valor
+let comidaFavorita
+console.log(comidaFavorita)
+
+//Atribuir valor a sua comida favorita
+comidaFavorita = 'mucunzá salgado';
+console.log(comidaFavorita)
+
+//nome = "Julio";
+
+nome = prompt('Digite seu nome'); // vai aparecer uma janelinha como oexemplo de "alert("Hello Word")
+
+console.log(nome)
+
+//String
+
+nome = "Nayara"
+let idade = "29"
+console.log(typeof(idade));
+
+//Number
+
+idade = 30;
+let altura = 1.53
+
+console.log(typeof(altura))
+
+
+// Boolean (true or false)
+
+const desenvolvedoraJavascript = true //camelcase é o *desevolvedoraJavascript, é o nome da variável, que é tudo junto
+
+
+
