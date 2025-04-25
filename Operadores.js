d9fd1c90abf6d78350d3835e88b768b659e89a06
Luanny <luanny>
2025-04-25 09:23:45 -0300

    Operadores.js

---
+++ Operadores.js
@@ -0,0 +1,66 @@
+//deixe numero1 = Number(prompt("Digite seu número"))
+//deixe numero2 = Number(prompt("Digite outro número"))
+
+
+//Soma
+//deixe soma = numero1 + numero2
+//Subtração
+//deixe subtração = numero1 - numero2
+// Multiplicação
+// deixe multiplicação = numero 1 * numero 2
+// Divisão
+//deixe divisão = numero1 / numero2
+
+
+//console.log(`A soma é ${soma}, a subtração é ${subtração}, a multiplicação é ${multiplicação} e a divisão é ${divisão}`)
+
+//outraSoma = "20" + 2 = 202 
+//let outraSubtração = "20" - 2 = a conversão ocorre para outros casos, mas não para subtração
+
+//operadores de atribuição (= ; += ; -= ; *= ; /= )
+
+//dado = "alguma informação"
+
+deixe x = 5;
+console.log(x)
+x = x + 5 // Resposta é 10
+console.log(x)
+x = x += 5 //Resposta é 15
+console.log(x)
+
+// Operadores de comparação (> ; < ; >= ; <= ; == ; != ; === ; !==) retorno booleano (True or False)
+
+console.log(3>6) // Falso
+console.log(10>4) //Verdadeiro
+console.log(10*3 >= 10/3) //Verdadeiro
+console.log(2 == "2") //Verdadeiro
+console.log(2 ==="2") //Falso                    OBS:"==" NÃO É IGUAL "==="
+console.log(2!=="2") //Verdadeiro
+
+// Operadores Lógicos (&& ; || ; !)    && = e(vale pros dois) ; || = ou (apenas um)
+
+console.registro(10 == 1 && 20 != 5) //As duas afirmaçoes devem ser verdadeira para ser True
+console.registro(20 == 20 && 3 < 10) //True
+console.registro(10 === "10" || 20 > 5) // True
+
+deixar x = 10
+deixar e = 20
+
+comparação = x > e //False
+console.registro(!!comparação) //Falsidade
+
+// Verdade ou Falsidade
+
+deixar nome = "" //Falsidade
+
+console.registro = (!!nome)
+
+
+
+
+
+
+
+
+
+
